5b89635dec429a6595ea62e7a79e0e9a20d00843
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-05 16:07:10 -0300

    feat: add fluxo updateUser completo

---
+++ src/Financeasy.Application/UseCases/UpdateUser/UpdateUserCommandValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+
+namespace Financeasy.Application.UseCases.UpdateUser
+{
+    public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserCommandValidator()
+        {
+            RuleFor(x => x.User.Email)
+                .EmailAddress().WithMessage("Email inválido.");
+
+            RuleFor(x => x.User.NewPassword)
+                .NotEmpty().WithMessage("Nova senha é obrigatória.")
+                .When(x => !string.IsNullOrWhiteSpace(x.User.OldPassword));
+
+            RuleFor(x => x.User.OldPassword)
+                .NotEmpty().WithMessage("Senha antiga é obrigatória.")
+                .When(x => !string.IsNullOrWhiteSpace(x.User.NewPassword));
+        }
+    }
+}
