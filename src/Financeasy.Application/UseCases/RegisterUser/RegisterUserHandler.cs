0a748111550697d6e7f60bb635007ea767067e3c
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-05 16:08:31 -0300

    fix: corrigindo uso async do userRepository e adicionando hasher de senha no fluxo do registerUser

--- src/Financeasy.Application/UseCases/RegisterUser/RegisterUserHandler.cs
+++ src/Financeasy.Application/UseCases/RegisterUser/RegisterUserHandler.cs
@@ -7,23 +7,25 @@ namespace Financeasy.Application.UseCases.RegisterUser
     public class RegisterUserHandler : IRequestHandler<RegisterUserCommand, Guid>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
 
-        public RegisterUserHandler(IUserRepository userRepository)
+        public RegisterUserHandler(IUserRepository userRepository, IPasswordHasher passwordHasher)
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
         }
 
-        public Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
         {
-            var alreadyExist = _userRepository.GetUserByEmail(request.Email);
+            var alreadyExist = await _userRepository.GetUserByEmail(request.Email);
 
             if(alreadyExist is not null)
                 throw new ArgumentException("Já existe um usuário com esse email");
 
-            var newUser = new User(request.Email, request.Password);
-            _userRepository.AddUser(newUser);
+            var newUser = new User(request.Email, _passwordHasher.Hash(request.Password));
+            await _userRepository.AddUser(newUser);
 
-            return Task.FromResult(newUser.Id);
+            return newUser.Id;
         }
     }
 }
