a0652c4f87e276ed7c789a979a76004398cc8d20
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-05 17:14:21 -0300

    fix: ajustado userRepository para usar o baseRepository

--- src/Financeasy.Domain/interfaces/IUserRepository.cs
+++ src/Financeasy.Domain/interfaces/IUserRepository.cs
@@ -2,12 +2,8 @@ using Financeasy.Domain.models;
 
 namespace Financeasy.Domain.interfaces
 {
-    public interface IUserRepository
+    public interface IUserRepository : IBaseRepository<User>
     {
-        Task AddUser(User user);
         Task<User?> GetUserByEmail(string email);
-        Task<User?> GetUserById(Guid id);
-        public void UpdateUser(User user);
-        public void DeleteUser(User user);
     }
 }
