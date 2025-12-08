004f9813ef2d2a7d04fe39b2c9b6f6e3f2259239
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-08 10:39:41 -0300

    feat: add lógica a handler de deleteFinancial

--- src/Financeasy.Application/UseCases/FinancialEntryCases/DeleteFinancialEntry/DeleteFinancialEntryHandler.cs
+++ src/Financeasy.Application/UseCases/FinancialEntryCases/DeleteFinancialEntry/DeleteFinancialEntryHandler.cs
@@ -1,12 +1,33 @@
+using Financeasy.Domain.interfaces;
 using MediatR;
 
 namespace Financeasy.Application.UseCases.FinancialEntryCases.DeleteFinancialEntry
 {
     public class DeleteFinancialEntryHandler : IRequestHandler<DeleteFinancialEntryCommand, DeleteFinancialEntryCommand>
     {
-        public Task<DeleteFinancialEntryCommand> Handle(DeleteFinancialEntryCommand request, CancellationToken cancellationToken)
+        private readonly IFinancialEntryRepository _financialRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteFinancialEntryHandler(IFinancialEntryRepository financialRepository, IUnitOfWork unitOfWork)
         {
-            throw new NotImplementedException();
+            _financialRepository = financialRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<DeleteFinancialEntryCommand> Handle(DeleteFinancialEntryCommand request, CancellationToken cancellationToken)
+        {
+            var financialExist = await _financialRepository.GetByIdAsync(request.Id);
+
+            if(financialExist is null)
+                throw new ArgumentException($"Não foi encontrado lançamento financeiro com o id {request.Id}");
+
+            if(financialExist.UserId != request.UserId)
+                throw new UnauthorizedAccessException("Usuário não possui acesso a essa ação.");
+
+            _financialRepository.Delete(financialExist);
+            await _unitOfWork.SaveChangesAsync();
+
+            return request;
         }
     }
 }
