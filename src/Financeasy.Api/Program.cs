231da8c120ba3aedc0bedf74377fbdb6c8228d02
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-10 15:05:59 -0300

    fix: ajuste no cors para permitir qualquer metodo

--- src/Financeasy.Api/Program.cs
+++ src/Financeasy.Api/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:5173/");
         policy.AllowAnyHeader();
         policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
     });
 });
 
