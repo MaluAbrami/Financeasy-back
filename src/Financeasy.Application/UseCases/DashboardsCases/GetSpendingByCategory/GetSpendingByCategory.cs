822237a03861fa4bea8266d80dd63cae01dda41a
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-11 18:35:56 -0300

    feat: criada estrutura da query, resposta e fluxo na handler para capturar valor total gasto com uma categoria

--- src/Financeasy.Application/UseCases/DashboardsCases/GetSpendingByCategory/GetSpendingByCategory.cs
+++ src/Financeasy.Application/UseCases/DashboardsCases/GetSpendingByCategory/GetSpendingByCategory.cs
@@ -4,11 +4,12 @@ namespace Financeasy.Application.UseCases.DashboardsCases.GetSpendingByCategory
 {
     public record GetSpendingByCategoryResponse()
     {
-        
+        public decimal TotalExpense { get; set; }
     }
 
     public record GetSpendingByCategory : IRequest<GetSpendingByCategoryResponse>
     {
-        
+        public Guid UserId { get; set; }
+        public required string Category { get; set; }
     }
 }
--- src/Financeasy.Application/UseCases/DashboardsCases/GetSpendingByCategory/GetSpendingByCategoryHandler.cs
+++ src/Financeasy.Application/UseCases/DashboardsCases/GetSpendingByCategory/GetSpendingByCategoryHandler.cs
@@ -1,12 +1,31 @@
+using Financeasy.Domain.interfaces;
 using MediatR;
 
 namespace Financeasy.Application.UseCases.DashboardsCases.GetSpendingByCategory
 {
     public class GetSpendingByCategoryHandler : IRequestHandler<GetSpendingByCategory, GetSpendingByCategoryResponse>
     {
+        private readonly IFinancialEntryRepository _financialRepository;
+        private readonly IUserRepository _userRepository;
+
+        public GetSpendingByCategoryHandler(IFinancialEntryRepository financialRepository, IUserRepository userRepository)
+        {
+            _financialRepository = financialRepository;
+            _userRepository = userRepository;
+        }
+        
         public async Task<GetSpendingByCategoryResponse> Handle(GetSpendingByCategory request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var userExist = _userRepository.GetByIdAsync(request.UserId);
+            if(userExist is null)
+            {
+                throw new UnauthorizedAccessException($"Usuário autenticado com id {request.UserId} não encontrado");
+            }
+
+            GetSpendingByCategoryResponse response = new();
+
+            response.TotalExpense = await _financialRepository.GetTotalExpenseByCategory(request.Category, request.UserId);
+            return response;
         }
     }
 }
