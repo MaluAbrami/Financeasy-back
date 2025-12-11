6e2013099550492a96d1c5a077ae7b0245f514c7
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-11 18:32:46 -0300

    feat: estruturado query para consulta de balanço geral, resposta e fluxo da handler

--- src/Financeasy.Application/UseCases/DashboardsCases/GetFinancialSummary/GetFinancialSummary.cs
+++ src/Financeasy.Application/UseCases/DashboardsCases/GetFinancialSummary/GetFinancialSummary.cs
@@ -4,11 +4,13 @@ namespace Financeasy.Application.UseCases.DashboardsCases.GetFinancialSummary
 {
     public record GetFinancialSummaryResponse()
     {
-        
+        public decimal TotalExpense { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal TotalBalance { get; set; }
     }
 
     public record GetFinancialSummary : IRequest<GetFinancialSummaryResponse >
     {
-        
+        public Guid UserId { get; set; }
     }
 }
