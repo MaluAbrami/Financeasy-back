e3752a8f97d8f02232dc146eb82465289a38516d
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-11 18:38:26 -0300

    feat: criado estrutura da query, resposta e fluxo na handler para capturar gasto total em um mês especifico

--- src/Financeasy.Application/UseCases/DashboardsCases/GetSpendingByMonth/GetSpendingByMonth.cs
+++ src/Financeasy.Application/UseCases/DashboardsCases/GetSpendingByMonth/GetSpendingByMonth.cs
@@ -4,11 +4,13 @@ namespace Financeasy.Application.UseCases.DashboardsCases.GetSpendingByMonth
 {
     public record GetSpendingByMonthResponse()
     {
-        
+        public decimal TotalExpense { get; set; }
     }
 
     public record GetSpendingByMonth : IRequest<GetSpendingByMonthResponse>
     {
-        
+        public Guid UserId { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
     }
 }
