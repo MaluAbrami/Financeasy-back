086628df3338ca08020805fec7a661f0e8740178
Malu Abrami <marialuizaabrami@gmail.com>
2025-12-12 12:42:55 -0300

    feat: add endpoints de capturar valor gasto por categoria e outro por mês

--- src/Financeasy.Api/Endpoints/DashboardsEndpoint.cs
+++ src/Financeasy.Api/Endpoints/DashboardsEndpoint.cs
@@ -1,5 +1,7 @@
 using Financeasy.Application.UseCases.DashboardsCases.GetBalanceEvolution;
 using Financeasy.Application.UseCases.DashboardsCases.GetFinancialSummary;
+using Financeasy.Application.UseCases.DashboardsCases.GetSpendingByCategory;
+using Financeasy.Application.UseCases.DashboardsCases.GetSpendingByMonth;
 using MediatR;
 using Microsoft.AspNetCore.Http.HttpResults;
 using MySqlX.XDevAPI.Common;
@@ -13,6 +15,10 @@ namespace Financeasy.Api.Endpoints
             group.MapGet("/financial-summary", GetFinancialSummary)
                 .RequireAuthorization();
             group.MapGet("/balance-evolution/{initialYear}/{initialMonth}/{endYear}/{endMonth}", GetBalanceEvolution)
+                .RequireAuthorization();
+            group.MapGet("/spending-by-category/{category}", GetSpendingByCategory)
+                .RequireAuthorization();
+            group.MapGet("spending-by-month/{year}/{month}", GetSpendingByMonth)
                 .RequireAuthorization();
 
             return group;
@@ -44,6 +50,28 @@ namespace Financeasy.Api.Endpoints
                     EndMonthComparison = endMonth
                 }
             );
+            return Results.Ok(response);
+        }
+
+        private async static Task<IResult> GetSpendingByCategory(string category, HttpContext context, IMediator mediator)
+        {
+            var userId = context.User.FindFirst("userId")?.Value;
+            if(userId is null)
+                return Results.Unauthorized();
+
+            var response = await mediator.Send(new GetSpendingByCategory { UserId = Guid.Parse(userId), Category = category });
+
+            return Results.Ok(response);
+        }
+
+        private async static Task<IResult> GetSpendingByMonth(int year, int month, HttpContext context, IMediator mediator)
+        {
+            var userId = context.User.FindFirst("userId")?.Value;
+            if(userId is null)
+                return Results.Unauthorized();
+
+            var response = await mediator.Send(new GetSpendingByMonth { UserId = Guid.Parse(userId), Year = year, Month = month });
+
             return Results.Ok(response);
         }
     }
