e3042a3d063416164ee3404784f494ad68355762
Malu Abrami <marialuizaabrami@gmail.com>
2026-01-14 10:30:25 -0300

    refactor: deletando o que será refeito

--- src/Financeasy.Application/Services/IUpdateExecutionService.cs
+++
@@ -1,9 +0,0 @@
-using Financeasy.Domain.models;
-
-namespace Financeasy.Application.Services
-{
-    public interface IUpdateExecutionService
-    {
-        Task<Update> ExecuteAsync(Guid userId, DateTime executionDate, CancellationToken ct);
-    }
-}
